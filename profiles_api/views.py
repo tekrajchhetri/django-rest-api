d1066dc13bae8dfbcdc428f89fe9575270f62d3d
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2020-02-11 16:37:06 +0200

    Post method and serializer added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import  Response
+from rest_framework import status
+from profiles_api import serializer
 
 class HelloAPIView(APIView):
     """Test API view"""
+
+    serializer_class = serializer.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -14,3 +18,15 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message':'hello','api_view':apiview})
+
+    def post(self, request):
+        """create hello message with our name"""
+        serializer= self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}"
+            return Response({"message":message})
+        else:
+            return  Response( serializer.errors,
+                status = 400)
