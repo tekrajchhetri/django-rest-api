8909a4596216e6388c0d2ee5931da6e9e3d348bc
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2020-02-12 09:22:40 +0200

    added profile viewsets

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -20,6 +20,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewset, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
    path('hello-viewapi/',views.HelloAPIView.as_view()),
